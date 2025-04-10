cfc5f1e05ed5fa844f6d133dbf265cd8d83f7afc
Jair Mena <jair.mena.remote@gmail.com>
2025-04-10 17:11:03 -0400

    Maquetacion examen 3 completo

---
+++ exTech.js
@@ -0,0 +1,19 @@
+// exTech.js
+
+let currentIndex = 0;
+const images = document.querySelectorAll('.carousel-image');
+
+function showImage(index) {
+    images.forEach((img, i) => {
+        img.classList.toggle('active', i === index);
+    });
+}
+
+function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+}
+
+setInterval(nextImage, 3000);
+
+showImage(currentIndex);
